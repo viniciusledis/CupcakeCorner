6eae72310641bc127de0a8927c160732321ab10a
Vinicius Ledis <161168846+viniciusledis@users.noreply.github.com>
2024-04-01 09:21:01 -0300

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Vinicius Ledis on 01/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
